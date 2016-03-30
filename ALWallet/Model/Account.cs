1512b0c7995bd14fe83b9d552159c10474927ff5
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-03-30 12:32:28 +0100

    Account.cs -> restored

--- ALWallet/Model/Account.cs
+++ ALWallet/Model/Account.cs
@@ -3,8 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite.Net.Attributes;
 
 namespace ALWallet.Model {
     class Account {
+
+        [PrimaryKey, AutoIncrement, NotNull]
+        public int id { get; set; }
+        [NotNull]
+        public string accname { get; set; }
+        [NotNull]
+        public double start { get; set; }
+        public double current { get; set; }
+        [NotNull]
+        public DateTime created { get; set; }
+
+        // constructor
+        public Account() { }
+
+        public Account(double current) {
+            this.current = current;
+        }
+
+        public Account(string accname, double start) {
+            this.accname = accname;
+            this.start = start;
+        }
+        public Account(string accname, double start, double current, DateTime created) {
+            this.accname = accname;
+            this.start = start;
+            this.current = current;
+            this.created = created;
+        }
     }
 }
