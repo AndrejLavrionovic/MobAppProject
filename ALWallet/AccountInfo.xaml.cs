c5fd192a90ae43b47a10abc4fd1c9077a52d38a7
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-03-30 13:37:20 +0100

    AccountInfo -> restored

--- ALWallet/AccountInfo.xaml.cs
+++ ALWallet/AccountInfo.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ALWallet.Control;
+using ALWallet.Model;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,8 +22,57 @@ namespace ALWallet {
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class AccountInfo : Page {
+
+        private App myApp;
+        private string accName;
+        private double curretn;
+        private double expenses;
+        private double income;
+        private double dept;
+        private double lend;
+        private Account a;
+        private DataContext dc;
+
         public AccountInfo() {
             this.InitializeComponent();
+
+            myApp = (Application.Current) as App;
+            dc = new DataContext();
+            a = dc.getAccount(myApp._accountId);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            tblAccountName.Text = String.Format("{0}", a.accname);
+            tblBalance.Text = String.Format("{0:c}", a.current);
+            tblExpenses.Text = String.Format("{0:c}", dc.getAllAmmount(myApp._accountId, DateTime.Now, "w"));
+            tblIncome.Text = String.Format("{0:c}", dc.getAllAmmount(myApp._accountId, DateTime.Now, "d"));
+            tblDebt.Text = String.Format("{0:c}", dc.getDebtsSum(myApp._accountId));
+            tblLend.Text = String.Format("{0:c}", dc.getLendsSum(myApp._accountId));
+        }
+
+        private void lbIcons_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+            if (lbiBack.IsSelected) {
+                myApp._accountId = 0;
+                myApp._accountName = "";
+                this.Frame.Navigate(typeof(HomePage));
+            }
+            else if (lbiHome.IsSelected) {
+                this.Frame.Navigate(typeof(AccountInfo));
+            }
+            else if (lbiTransactions.IsSelected) {
+                this.Frame.Navigate(typeof(Transactions));
+            }
+            else if (lbiBorrow.IsSelected) {
+                this.Frame.Navigate(typeof(Borrowed));
+            }
+            else if (lbiLend.IsSelected) {
+                this.Frame.Navigate(typeof(Lent));
+            }
+        }
+
+        private void btnHamburger_Click(object sender, RoutedEventArgs e) {
+            splSideMenu.IsPaneOpen = !splSideMenu.IsPaneOpen;
         }
     }
 }
