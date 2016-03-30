faddd124cd3d97559ba24e51ad20bbbbee5cf01b
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-03-30 12:36:37 +0100

    LendMod.cs -> restored

--- ALWallet/Model/LendMod.cs
+++ ALWallet/Model/LendMod.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite.Net.Attributes;
 
 namespace ALWallet.Model {
     class LendMod {
+
+        [PrimaryKey, NotNull, AutoIncrement]
+        public int id { get; set; }
+        [NotNull]
+        public string to { get; set; }
+        [NotNull]
+        public DateTime date { get; set; }
+        [NotNull]
+        public double ammount { get; set; }
+        [NotNull]
+        public int accid { get; set; }
+
+        // constructors
+        public LendMod() { }
+
+        public LendMod(string to, DateTime date, double ammount) {
+            this.to = to;
+            this.date = date;
+            this.ammount = ammount;
+        }
     }
 }
