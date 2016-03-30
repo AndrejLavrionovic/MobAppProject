f574ca01eda29df29489911cb3982554b4fb6ba4
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-03-30 12:33:44 +0100

    Categories.cs -> restored

--- ALWallet/Model/Categories.cs
+++ ALWallet/Model/Categories.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite.Net.Attributes;
 
 namespace ALWallet.Model {
     class Categories {
+
+        [PrimaryKey, NotNull, AutoIncrement]
+        public int id { get; set; }
+        [NotNull]
+        public string name { get; set; }
+
+        // constructors
+        public Categories() { }
+        public Categories(string name) {
+            this.name = name;
+        }
     }
 }
