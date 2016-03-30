ed7097fb1f38bd05072e69e493a4c36db330ae15
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-03-30 13:23:38 +0100

    NewAccount -> restored

--- ALWallet/NewAccount.xaml.cs
+++ ALWallet/NewAccount.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ALWallet.Model;
+using ALWallet.Control;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,6 +24,104 @@ namespace ALWallet {
     public sealed partial class NewAccount : Page {
         public NewAccount() {
             this.InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e) {
+            if (Frame.CanGoBack)
+                Frame.GoBack();
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e) {
+
+            if (isValid()) {
+
+                DataContext dc = new DataContext();
+
+                // account name
+                string accName = tbxAccount.Text;
+                double balance = Convert.ToDouble(tbxBalance.Text);
+                DateTime date = DateTime.Now;
+
+                // instnce of Account
+                Account account = new Account(accName, balance, balance, date);
+
+                if (dc.createNewAccount(account) == 1) {
+                    this.Frame.Navigate(typeof(HomePage));
+                }
+                else {
+                    spErrors.Visibility = Visibility.Visible;
+                    TextBlock tblErr = new TextBlock();
+                    tblErr.Text = "Error: Account creation is faild.";
+                }
+
+                // include account into global list
+                //addAccount(account);
+
+                tbxAccount.Text = "";
+                tbxBalance.Text = "";
+
+                // Navigate to transactions
+            }
+        }
+
+        private bool isValid() {
+
+            // check if spErrors is visible end make is not visible if it is.
+            // alse if spErrors has childs then remove all.
+            if (spErrors.Visibility == Visibility.Visible) {
+                spErrors.Visibility = Visibility.Collapsed;
+                spErrors.Children.Clear();
+            }
+
+            // validation
+            int err = 0;
+            int[] error = { 0, 0, 0 };
+            double result = 0.00;
+            if (!Double.TryParse(tbxBalance.Text, out result)) {
+                err++;
+                error[2] = 1;
+            }
+            if (tbxBalance.Text == "") {
+                err++;
+                error[1] = 1;
+            }
+            if (tbxAccount.Text == "") {
+                err++;
+                error[0] = 1;
+            }
+
+            if (err > 0) {
+                TextBlock tblErr;
+
+                if (error[0] == 1) {
+                    tblErr = new TextBlock();
+                    tblErr.Text = "Account name must be entered.";
+                    tblErr.Height = 30;
+
+                    spErrors.Children.Add(tblErr);
+                }
+                if (error[1] == 1) {
+                    tblErr = new TextBlock();
+                    tblErr.Text = "Starting balance must be entered.";
+                    tblErr.Height = 30;
+
+                    spErrors.Children.Add(tblErr);
+                }
+                if (error[2] == 1) {
+                    tblErr = new TextBlock();
+                    tblErr.Text = "Bad value. Number must be entered into balance field.";
+                    tblErr.Height = 30;
+
+                    spErrors.Children.Add(tblErr);
+                }
+                spErrors.Visibility = Visibility.Visible;
+                return false;
+            }
+            else return true;
+        }
+
+        private void addAccount(Account acc) {
+            DataContext dc = new DataContext();
         }
     }
 }
