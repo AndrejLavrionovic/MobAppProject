9d54cf1cbbb18966ee7fc17dd994faba3a71a9ef
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-03-30 12:54:11 +0100

    MainPage.xaml + MainPage.xaml.cs -> restored

--- ALWallet/MainPage.xaml.cs
+++ ALWallet/MainPage.xaml.cs
@@ -26,5 +26,9 @@ namespace ALWallet
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(HomePage));
+        }
     }
 }
