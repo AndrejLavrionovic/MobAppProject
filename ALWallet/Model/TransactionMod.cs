a7f4958157bb6b6cd9a1d69a23aaa312f2e0902f
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-03-30 12:37:45 +0100

    TransactionMod.cs -> restored

--- ALWallet/Model/TransactionMod.cs
+++ ALWallet/Model/TransactionMod.cs
@@ -3,8 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite.Net.Attributes;
 
 namespace ALWallet.Model {
     class TransactionMod {
+
+        [PrimaryKey, NotNull, AutoIncrement]
+        public int id { get; set; }
+        [NotNull]
+        public string type { get; set; }
+        [NotNull]
+        public DateTime date { get; set; }
+        public int categoryid { get; set; }
+        public string description { get; set; }
+        [NotNull]
+        public double ammount { get; set; }
+        [NotNull]
+        public int accid { get; set; }
+
+        // constructors
+        public TransactionMod() { }
+
+        public TransactionMod(string type, DateTime date, double ammount, int accid) {
+            this.type = type;
+            this.date = date;
+            this.ammount = ammount;
+            this.accid = accid;
+        }
+
+        public TransactionMod(string type, DateTime date, double ammount, int accid, int categoryid) {
+            this.type = type;
+            this.date = date;
+            this.ammount = ammount;
+            this.accid = accid;
+            this.categoryid = categoryid;
+        }
+
+        public TransactionMod(string type, DateTime date, double ammount, int accid, int categoryid, string description) {
+            this.type = type;
+            this.date = date;
+            this.ammount = ammount;
+            this.accid = accid;
+            this.categoryid = categoryid;
+            this.description = description;
+        }
     }
 }
