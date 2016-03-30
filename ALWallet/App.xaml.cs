c97b5c972f40c48f33097a8953c8e077492fdf38
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-03-30 12:20:24 +0100

    SQLite references was included

--- ALWallet/App.xaml.cs
+++ ALWallet/App.xaml.cs
@@ -22,6 +22,10 @@ namespace ALWallet
     /// </summary>
     sealed partial class App : Application
     {
+
+
+        public int _accountId { get; set; }
+        public string _accountName { get; set; }
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
